f9b7f35f65fd4acf810100ba5faa7d9a1b4b1721
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2022-01-21 01:10:59 +0900

    flush

--- jpa_basic/src/main/java/hellojpa/JpaMain.java
+++ jpa_basic/src/main/java/hellojpa/JpaMain.java
@@ -15,35 +15,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            // 엔티티를 생성한 상태 (비영속)
-//            Member member = new Member();
-//            member.setId(100L);
-//            member.setName("회원1");
-//
-//            // 영속 (아직 DB에 저장되지 않음)
-//            System.out.println("=== BEFORE ===");
-//            // 1차 캐시에 저장됨
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
+            Member member = new Member(1000L, "member1000");
+            em.persist(member);
 
-            // DB가 아닌 1차 캐시에서 가져옴
-            // DB에 있는 경우 select문으로 1차 캐시로 가져옴
-            // findMember1에서 select문으로 DB에서 가져오고, findMember2에서는 1차 캐시에서 가져온다.
-            Member findMember1 = em.find(Member.class, 150L);
-            Member findMember2 = em.find(Member.class, 150L);
+            // 1차 캐시는 유지된다.
+            em.flush();
 
-
-            System.out.println("findMember.getId() = " + findMember1.getId());
-            System.out.println("findMember.getName() = " + findMember1.getName());
-            Member member1 = new Member(150L, "memberA");
-            Member member2 = new Member(160L, "memberB");
-
-            em.persist(member1);
-            em.persist(member2);
-
-//            System.out.println("======================");
-//
-//            // 커밋하는 순간 DB에 INSERT SQL을 보낸다.
+            System.out.println("===================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
